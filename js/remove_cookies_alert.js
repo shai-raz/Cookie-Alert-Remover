7073e631a5c9af21ca5548f1ca2c47bca5391a36
Shai <shayy9999@gmail.com>
2021-02-23 12:55:01 +0200

    now only applies to div/span/p

--- js/remove_cookies_alert.js
+++ js/remove_cookies_alert.js
@@ -2,26 +2,31 @@ const createSelectorString = (keywords)=> {
     let str = "";
 
     keywords.forEach(function(keyword) {
-        str += "[class*="+keyword+"],";
-        str += "[id*="+keyword+"],";
+        str += "div[class*="+keyword+"],";
+        str += "span[class*="+keyword+"],";
+        str += "p[class*="+keyword+"],";
+        str += "div[id*="+keyword+"],";
+        str += "span[id*="+keyword+"],";
+        str += "p[id*="+keyword+"],";
 
         let capped = keyword.charAt(0).toUpperCase() + keyword.slice(1);
-        str += "[class*="+capped+"],";
-        str += "[id*="+capped+"],";
+        str += "div[class*="+capped+"],";
+        str += "span[class*="+capped+"],";
+        str += "p[class*="+capped+"],";
+        str += "div[id*="+capped+"],";
+        str += "span[id*="+capped+"],";
+        str += "p[id*="+capped+"],";
     })
 
-    return str.slice(0, -1);
+    return str.slice(0, -1); // remove last comma
 }
 
 const keywords = ["cookie", "ccpa", "gdpr"];
 const selectorString = createSelectorString(keywords);
-console.log("Mark Zuckerberg is sending his regards.");
+console.log("Mark Zuckerberg sends his regards.");
 
 const removeCookieAlert = ()=> {
     $(selectorString).remove();
-    /*while ($(selectorString).length) {
-        $(selectorString).remove();
-    }*/
 };
 
 $(function(){
