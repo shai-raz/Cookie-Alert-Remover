6c805cf72c792e1d7bfb19152887db74a2b5baa2
Shai <shayy9999@gmail.com>
2021-02-23 13:16:58 +0200

    now disabled for pages with cookie keywords in address

--- js/remove_cookies_alert.js
+++ js/remove_cookies_alert.js
@@ -1,11 +1,14 @@
 /* functions */
-String.prototype.includesFromArr = (arr)=> {
-    for (let i = 0; i < arr.length; i++) {
-        if (this.includes(arr[i]))
-            return true;
+Object.defineProperty(String.prototype, 'includesFromArr', {
+    value: function(arr) {
+        console.log(this.toString())
+        for (let i = 0; i < arr.length; i++) {
+            if (this.includes(arr[i]))
+                return true;
+        }
+        return false;
     }
-    return false;
-}
+})
 
 const createSelectorString = (keywords)=> {
     let str = "";
@@ -44,6 +47,7 @@ console.log("Mark Zuckerberg sends his regards.");
 
 /* on doc ready / on window load */
 $(function(){
+    //alert(location.href)
     if ((location.href).includesFromArr(keywords)) {
         cookiePage = true;
         return;
